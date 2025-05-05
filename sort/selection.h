5edae2e751ecbd5697086aafe1b669321f56faae
Mateus Vieira <habdig7@gmail.com>
2025-05-05 11:24:56 -0300

    terminar dps

--- sort/selection.h
+++ sort/selection.h
@@ -2,7 +2,21 @@
 
 int *min(int arr[], int len){
     int *min = &arr[0];
-    for(int i  = 0; i < len; i++)
+    for(int i = 0; i < len; i++)
         if(arr[i] < *min)
             min = &arr[i];
+    return min;
+}
+
+int selection_sort(int arr[], int len){
+    for(int i = 0; i < len - 1; i++){
+        int *v_min = min(&arr[i], len-i);
+        printf("Vmin: %d\n", *v_min);
+        int aux = *v_min;
+        *v_min = arr[i];
+        arr[i] = aux;
+
+        for(int j = 0; j < len; j++)
+        printf("%d ", arr[j]);
+    }
 }
--- tests/selection/test_selection.h
+++ tests/selection/test_selection.h
@@ -2,19 +2,23 @@
 #include "stdio.h"
 
 void find_min(){
+    printf("--------------- FIND MIN ---------------");
     int arr[] = {5, 2, 4, 10, 2, 1};
     int len = sizeof(arr) / sizeof(arr[0]);
     int *v_min = min(arr, len);
 
-    printf("%d", *v_min);
+    printf("\n%d", *v_min);
 }
 
-/*
 void ex4(){
-    int arr[] = {5, 2, 4, 10, 2, 1};
+    printf("------------- SELECTION -------------\n");
+    int arr[] = {5, 11, 4, 10, 1};
+    int sorted[] = {1, 2, 4, 5, 10,};
     int len = sizeof(arr) / sizeof(arr[0]);
+
     selection_sort(arr, len);
     for(int i = 0; i < len; i++){
         printf("%d\n", arr[i]);
+      //  assert(arr[i] == sorted[i]);
     }
-}*/
+}
