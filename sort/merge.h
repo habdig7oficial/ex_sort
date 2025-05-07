bfb7d45956593837746bdbc673f72914c3c0495d
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-07 12:38:22 -0300

    pthread complete

--- sort/merge.h
+++ sort/merge.h
@@ -7,21 +7,32 @@ struct ArrArgs {
 
 void *merge_sort(void *ptr_args){
 
-    printf("Lorem %d\n\n\n", ((struct ArrArgs *) ptr_args) -> len);
+    struct ArrArgs args = *(struct ArrArgs *) ptr_args;
 
-    /*
-        printf("bg: \n");
-    for(int i = 0; i < len; i++)
-        printf("%d\n", arr[i]);
+    printf("bg: %d \n", args.len);
+    for(int i = 0; i < args.len; i++)
+        printf("%d\n", args.arr[i]);
     printf("end: \n");
-    
-    if(len == 1)
-        return &arr[0];
 
-    merge_sort(&arr[0], len / 2);             //direita
-    merge_sort(&arr[(len / 2) + 1], len / 2);              //right
+    if(args.len == 1)
+        return &args.arr[0];
 
- */
+    struct ArrArgs args_left = {
+        .arr = &args.arr[0],
+        .len = args.len / 2
+    };
+    struct ArrArgs args_right = {
+        .arr = &args.arr[(args.len / 2) + 1],
+        .len = args.len / 2
+    };
+    pthread_t th_1, th_2;
+
+    pthread_create(&th_1, NULL, &merge_sort, &args_left); // left
+    pthread_create(&th_2, NULL, &merge_sort, &args_right); // right
+
+    pthread_join(th_1, NULL);
+    pthread_join(th_2, NULL);
+
 
     /*
     for(int i = 0; i < len - 1; i++){
