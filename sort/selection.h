a067837ebb10289bd5f24061815e954a154d0989
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-11 14:43:23 -0300

    ok

--- sort/selection.h
+++ sort/selection.h
@@ -1,4 +1,4 @@
-
+#include "stdbool.h"
 
 int *min(int arr[], int len){
     int *min = &arr[0];
@@ -8,14 +8,34 @@ int *min(int arr[], int len){
     return min;
 }
 
-void selection_sort(int arr[], int len){
-    for(int i = 0; i < len - 1; i++){
-        int *v_min = min(&arr[i], len-i);
-        if(*v_min < arr[i]){
-            int aux = *v_min;
-            *v_min = arr[i];
-            arr[i] = aux;
-        }
+int *max(int arr[], int len){
+    int *v_max = &arr[0];
+    for(int i = 0; i < len; i++)
+        if(arr[i] > *v_max)
+            v_max = &arr[i];
+    return v_max;
+}
 
+
+void selection_sort(int arr[], int len, bool is_asc){
+    if(is_asc){
+        for(int i = 0; i < len - 1; i++){
+            int *v_min = min(&arr[i], len-i);
+            if(*v_min < arr[i]){
+                int aux = *v_min;
+                *v_min = arr[i];
+                arr[i] = aux;
+            }
+        }
+    }
+    else {
+        for(int i = 0; i < len - 1; i++){
+            int *v_max = max(&arr[i], len-i);
+            if(*v_max > arr[i]){
+                int aux = *v_max;
+                *v_max = arr[i];
+                arr[i] = aux;
+            }
+        }
     }
 }
--- tests/selection/test_selection.h
+++ tests/selection/test_selection.h
@@ -1,6 +1,6 @@
 #include "assert.h"
 #include "stdio.h"
-
+#include "stdbool.h"
 #include "../../helpers/delimiter.h"
 
 
@@ -14,16 +14,31 @@ void find_min(){
 }
 
 void ex4(){
-    hr("SELECTION", 15);
+    hr("SELECTION ASC", 15);
     int arr[] = {5, 2, 4, 10, 1, 2, 2, -1};
     int sorted[] = {-1, 1, 2, 2, 2, 4, 5, 10};
 
 
     int len = sizeof(arr) / sizeof(arr[0]);
 
-    selection_sort(arr, len);
+    selection_sort(arr, len, true);
     for(int i = 0; i < len; i++){
         printf("%d - %d\n", arr[i], sorted[i]);
         assert(arr[i] == sorted[i]);
+    }
+}
+
+void ex5(){
+    hr("SELECTION DESC", 15);
+    int arr[] = {5, 2, 4, 10, 1, 2, 2, -1};
+    int sorted[] = {10, 5, 4, 2, 2, 2, 1, -1};
+
+
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    selection_sort(arr, len, false);
+    for(int i = 0; i < len; i++){
+        printf("%d - %d\n", arr[i], sorted[i]);
+       assert(arr[i] == sorted[i]);
     }
 }
